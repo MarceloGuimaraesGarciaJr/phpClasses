19d8cef7214cf828f832fef2c4886de495387073
Marcelo <marceloggarciajr@gmail.com>
2023-05-24 17:25:35 -0300

    Aulas do dia 24/05
    Já na alura

---
+++ php/challenges/tabuada.php
@@ -0,0 +1,8 @@
+<?php
+$multiplicador = 8;
+
+for ($i = 1; $i <= 10; $i++ ){
+    $resultado = $i*$multiplicador;
+
+    echo ("O número $i multiplicado por $multiplicador é igual a $resultado \n");
+}
