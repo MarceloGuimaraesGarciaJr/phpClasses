ee54cb5e01f2cd565fb84949977547d5dd248721
Marcelo <marceloggarciajr@gmail.com>
2023-05-28 21:23:56 -0300

    Aulas do dia 25/05
    Já na alura

--- php/Avançando no PHP - Arrays, Strings, Função e Web/contas_aula.php
+++ php/Avançando no PHP - Arrays, Strings, Função e Web/contas_aula.php
@@ -1,5 +1,12 @@
 <?php
 
+
+function exibeMensagem($mensagem){
+    echo $mensagem . PHP_EOL;
+}
+
+
+
 $contasCorrentes = [
     '123.456.789-10' => [
         'titular' => 'Maria',
@@ -7,14 +14,36 @@ $contasCorrentes = [
     ],
     '123.456.689-11' => [
         'titular' => 'Alberto',
-        'saldo' => 300
+        'saldo' => 1000
     ],
     '123.256.789-12' => [
         'titular' => 'Vinicius',
         'saldo' => 100
     ]
 ];
+if (500 > $contasCorrentes['123.456.789-10']['saldo']){
+    exibeMensagem("Você não tem esse saldo");
+} else
+{
+    $contasCorrentes['123.456.789-10']['saldo'] -= 500;
+}
+
+if (500 > $contasCorrentes['123.456.689-11']['saldo']){
+    exibeMensagem("Você não tem esse saldo");
+} else
+{
+    $contasCorrentes['123.456.689-11']['saldo'] -= 500;
+}
+
+if (500 > $contasCorrentes['123.256.789-12']['saldo']){
+    exibeMensagem("Você não tem esse saldo");
+} else
+{
+    $contasCorrentes['123.256.789-12']['saldo'] -= 500;
+}
+
 
 foreach ($contasCorrentes as $cpf => $conta) {
-    echo $cpf . " " . $conta['titular'] . PHP_EOL;
+    exibeMensagem($cpf . " " . $conta['titular'] . " "."R$ " . $conta['saldo']);
 }
+
