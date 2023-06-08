9f856ae45d221c19662c61b8c14a23d874f25145
Marcelo <marceloggarciajr@gmail.com>
2023-06-08 15:54:20 -0300

    Criação das classes me PHP 07/06

---
+++ php/Programação Orientada a Objetos - PHP/src/cpf.php
@@ -0,0 +1,18 @@
+<?php
+
+class cpf
+{
+    private string $cpf;
+
+    public function __construct(string $cpf)
+    {
+        $this->cpf = $cpf;
+    }
+    public function getCpf():string
+    {
+        return $this->cpf;
+    }
+
+
+
+}
